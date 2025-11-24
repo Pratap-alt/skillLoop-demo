6774c119338e796f82c95ea0e6dd935818df2a3a
Pratap-alt <88791125+Pratap-alt@users.noreply.github.com>
2025-11-24 05:43:33 +0530

    Update mongoose.js

--- backend/lib/mongoose.js
+++ backend/lib/mongoose.js
@@ -3,7 +3,7 @@
 
 const mongoose = require('mongoose');
 
-const MONGODB_URI ='mongodb+srv://Pratap:Pratap@pratap.wykz1my.mongodb.net/'
+const MONGODB_URI =mongodb+srv://Pratap:Pratap@pratap.wykz1my.mongodb.net/?appName=pratap
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside Vercel.');
