00d68c75f1dd946541f35784c76ebe13d161f47c
Preetom <167507439+PreetomBoro@users.noreply.github.com>
2025-11-25 01:32:27 +0530

    login -> index correction

--- public/js/logout.js
+++ public/js/logout.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
         localStorage.removeItem("skillloop_user");
         localStorage.removeItem("skillloop_token");
         document.cookie = "skillloop_token=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-        window.location.href = "login.html";
+        window.location.href = "index.html";
         });
     });
 });
